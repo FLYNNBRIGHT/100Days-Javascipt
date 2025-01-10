03b7fb79d290af6178eaab18dc10b56ee6403999
FLYNNBRIGHT <flynnbright@gmail.com>
2025-01-10 20:36:19 +0100

    Started Bro Code Javascript Tutorial Again

---
+++ Bro Code Javascript/index.js
@@ -0,0 +1,8 @@
+let age  = 25;
+let firstName = "Matthew";
+let lastName = "Archibong";
+let myLocation = "Ikot  Ekpene";
+
+
+console.log(`My name is ${firstName} ${lastName} and I am ${age}years old. And I leave in ${myLocation}`)
+
